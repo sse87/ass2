35037c9734d99b84153f4a17c40d7d6ad412a645
bjarniegill <bjarniegill@gmail.com>
2014-10-03 23:27:48 +0000

    AddGradeToProject
    added AddGradeToProject post function to project

---
+++ CoursesAPI.Models/GradeDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoursesAPI.Models
+{
+    public class GradeDTO
+    {
+        public string studentName { get; set; }
+        public double grade { get; set; }
+        public string projectName { get; set; }
+        public string courseInstanceName { get; set; }
+    }
+}
