da89f1365e7753fd1de48a742048a8b71f9dc16e
Sigurður Snær Eiríksson <sse1987@gmail.com>
2014-10-04 13:59:31 +0000

    Just details

--- CoursesAPI/Controllers/CoursesController.cs
+++ CoursesAPI/Controllers/CoursesController.cs
@@ -7,28 +7,6 @@ using CoursesAPI.Services.Services;
 
 namespace CoursesAPI.Controllers
 {
-	[RoutePrefix("api/projectgroups")]
-	public class ProjectGroupController  : ApiController
-	{
-		private readonly CoursesServiceProvider _service;
-
-		public ProjectGroupController()
-		{
-			_service = new CoursesServiceProvider(new UnitOfWork<AppDataContext>());
-		}
-
-		public List<ProjectGroup> GetProjectGroups()
-		{
-			return _service.GetProjectGroups();
-		}
-
-		[HttpPost]
-		public ProjectGroup AddProjectGroup(ProjectGroup model)
-		{
-			return _service.AddProjectGroup(model);
-		}
-	}
-
 	[RoutePrefix("api/courses")]
 	public class CoursesController : ApiController
 	{
@@ -61,12 +39,23 @@ namespace CoursesAPI.Controllers
 			return _service.GetCourseStudents(courseInstanceID);
 		}
 		
+		/// <summary>
+		/// Get all courses on a given semester
+		/// </summary>
+		/// <param name="semester"></param>
+		/// <returns></returns>
 		[Route("semester/{semester}")]
 		public List<CourseInstanceDTO> GetCoursesOnSemester(string semester)
 		{
 			return _service.GetSemesterCourses(semester);
 		}
 
+		/// <summary>
+		/// Add project to a course
+		/// </summary>
+		/// <param name="courseInstanceID"></param>
+		/// <param name="model"></param>
+		/// <returns></returns>
 		[HttpPost]
 		[Route("{courseInstanceID}/projects")]
 		public Project AddProjectToCourse(int courseInstanceID, Project model)
@@ -74,11 +63,39 @@ namespace CoursesAPI.Controllers
 			return _service.AddProjectToCourse(courseInstanceID, model);
 		}
 
-        [HttpPost]
-        [Route("{projectID}/grades")]
-         public GradeDTO AddGradeToProject(int projectID, AddGradeToProjectViewModel model)
-        {
-            return _service.AddGradeToProject(projectID, model);
-        }
+		/// <summary>
+		/// Get list of project groups
+		/// </summary>
+		/// <returns></returns>
+		[Route("projectgroup")]
+		public List<ProjectGroup> GetProjectGroups()
+		{
+			return _service.GetProjectGroups();
+		}
+
+		/// <summary>
+		/// Insert project group
+		/// </summary>
+		/// <param name="projectGroup"></param>
+		/// <returns></returns>
+		[HttpPost]
+		[Route("projectgroup")]
+		public ProjectGroup AddProjectGroup(ProjectGroup projectGroup)
+		{
+			return _service.AddProjectGroup(projectGroup);
+		}
+
+		/// <summary>
+		/// Add Grade to a project
+		/// </summary>
+		/// <param name="projectID"></param>
+		/// <param name="model"></param>
+		/// <returns></returns>
+		[HttpPost]
+		[Route("{projectID}/grades")]
+			public GradeDTO AddGradeToProject(int projectID, AddGradeToProjectViewModel model)
+		{
+			return _service.AddGradeToProject(projectID, model);
+		}
 	}
 }
